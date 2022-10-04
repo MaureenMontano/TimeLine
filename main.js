97a1a1dd86c27dcbf5fa53cbfcc85d4b35afc99f
Noe Cortez <corteznoe3@gmail.com>
2022-10-04 14:17:07 -0600

    Funcionalidad de cambio

--- main.js
+++ main.js
@@ -1,110 +1,130 @@
-//crear un arreglo que se llame movimientos sort buscar// 
+const btn = document.getElementById("btn");
 
-var btn = document.getElementById('btn')
+let filter = "DESC";
 
-function cambio()
-
-btn.addEventListener('click')
+btn.addEventListener("click", cambiarOrden);
 
 const movimientos = [
-    {
-        responsable: "Ventanilla Electrónica",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Ventanilla Electrónica envió una solicitud al sistema de pruebas con laboratorios externos.",
-    },
+  {
+    responsable: "Ventanilla Electrónica",
+    fecha: "2022-10-03",
+    descripcion:
+      "Ventanilla Electrónica envió una solicitud al sistema de pruebas con laboratorios externos.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Técnico DNM recibió una solicitud desde Ventanilla Electrónica.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-03",
+    descripcion:
+      "Técnico DNM recibió una solicitud desde Ventanilla Electrónica.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Técnico DNM acepta la solicitud.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-03",
+    descripcion: "Técnico DNM acepta la solicitud.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Técnico DNM elige laboratorio para realizar la prueba de análisis.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-03",
+    descripcion:
+      "Técnico DNM elige laboratorio para realizar la prueba de análisis.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Técnico DNM envía solicitud de cotización a laboratorio externo.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-03",
+    descripcion:
+      "Técnico DNM envía solicitud de cotización a laboratorio externo.",
+  },
 
-    {
-        responsable: "Laboratorio externo",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Laboratorio externo recibe solicitud de cotización.",
-    },
+  {
+    responsable: "Laboratorio externo",
+    fecha: "2022-10-03",
+    descripcion: "Laboratorio externo recibe solicitud de cotización.",
+  },
 
-    {
-        responsable: "Laboratorio externo",
-        fecha: "3 de octubre de 2022",
-        descripcion: "Laboratorio externo verifica la factibilidad de la prueba a realizar análisis de laboratorio.",
-    },
+  {
+    responsable: "Laboratorio externo",
+    fecha: "2022-10-03",
+    descripcion:
+      "Laboratorio externo verifica la factibilidad de la prueba a realizar análisis de laboratorio.",
+  },
 
-    {
-        responsable: "Laboratorio externo",
-        fecha: "5 de octubre de 2022",
-        descripcion: "Laboratorio externo responde la solicitud con una respuesta factible.",
-    },
+  {
+    responsable: "Laboratorio externo",
+    fecha: "2022-10-05",
+    descripcion:
+      "Laboratorio externo responde la solicitud con una respuesta factible.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "5 de octubre de 2022",
-        descripcion: "Técnico DNM recibe la respuesta de la cotización del laboratorio externo.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-05",
+    descripcion:
+      "Técnico DNM recibe la respuesta de la cotización del laboratorio externo.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "5 de octubre de 2022",
-        descripcion: "Técnico DNM envía las muestras necesarias al laboratorio externo.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-05",
+    descripcion:
+      "Técnico DNM envía las muestras necesarias al laboratorio externo.",
+  },
 
-    {
-        responsable: "Laboratorio externo",
-        fecha: "5 de octubre de 2022",
-        descripcion: "Laboratorio externo recibe las muestras para su debido análisis.",
-    },
+  {
+    responsable: "Laboratorio externo",
+    fecha: "2022-10-05",
+    descripcion:
+      "Laboratorio externo recibe las muestras para su debido análisis.",
+  },
 
-//analisis en proceso
+  //analisis en proceso
 
-    {
-        mensaje: "Análisis en proceso."
-    },
+  /* {
+    mensaje: "Análisis en proceso.",
+  }, */
 
-    {
-        responsable: "Laboratorio externo",
-        fecha: "31 de octubre de 2022",
-        descripcion: "Laboratorio externo finaliza el análisis y envía los resultados al Técnico DNM",
-    },
+  {
+    responsable: "Laboratorio externo",
+    fecha: "2022-10-31",
+    descripcion:
+      "Laboratorio externo finaliza el análisis y envía los resultados al Técnico DNM",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "31 de octubre de 2022",
-        descripcion: "Técnico DNM recibe mediante una solicitud los resultados del análisis de laboratorio.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-31",
+    descripcion:
+      "Técnico DNM recibe mediante una solicitud los resultados del análisis de laboratorio.",
+  },
 
-    {
-        responsable: "Técnico DNM",
-        fecha: "31 de octubre de 2022",
-        descripcion: "Técnico DNM envía los resultados a la ventanilla electrónica DNM.",
-    },
+  {
+    responsable: "Técnico DNM",
+    fecha: "2022-10-31",
+    descripcion:
+      "Técnico DNM envía los resultados a la ventanilla electrónica DNM.",
+  },
 
-    {
-        mensaje2: "Proceso finalizado."
-    }
+  /* {
+    mensaje2: "Proceso finalizado.",
+  }, */
 
-    //proceso finalizado
+  //proceso finalizado
 ];
 
-movimientos.sort()
-console.log(movimientos);
+function cambiarOrden() {
+  let movNew;
+  if (filter === "ASC") {
+    movNew = movimientos.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+    filter = 'DESC'
+    btn.textContent = 'Descendente';
+  } else {
+    movNew = movimientos.sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
+    filter = 'ASC';
+    btn.textContent = 'Ascendente';
+  }
 
-
+  console.log(movNew);
+}
