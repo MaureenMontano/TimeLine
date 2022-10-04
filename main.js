71afdf9cec34abfc1a293353df76933ebc2018b1
Noe Cortez <corteznoe3@gmail.com>
2022-10-04 14:46:54 -0600

    Dinamismo

--- main.js
+++ main.js
@@ -1,118 +1,141 @@
 const btn = document.getElementById("btn");
+const container = document.querySelector('.wrapper');
+const movimientoCard = document.createElement('div');
+
+const movimientos = [
+    {
+      responsable: "Ventanilla Electrónica",
+      fecha: "2022-10-03",
+      descripcion:
+        "Ventanilla Electrónica envió una solicitud al sistema de pruebas con laboratorios externos.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-03",
+      descripcion:
+        "Técnico DNM recibió una solicitud desde Ventanilla Electrónica.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-03",
+      descripcion: "Técnico DNM acepta la solicitud.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-03",
+      descripcion:
+        "Técnico DNM elige laboratorio para realizar la prueba de análisis.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-03",
+      descripcion:
+        "Técnico DNM envía solicitud de cotización a laboratorio externo.",
+    },
+  
+    {
+      responsable: "Laboratorio externo",
+      fecha: "2022-10-03",
+      descripcion: "Laboratorio externo recibe solicitud de cotización.",
+    },
+  
+    {
+      responsable: "Laboratorio externo",
+      fecha: "2022-10-03",
+      descripcion:
+        "Laboratorio externo verifica la factibilidad de la prueba a realizar análisis de laboratorio.",
+    },
+  
+    {
+      responsable: "Laboratorio externo",
+      fecha: "2022-10-05",
+      descripcion:
+        "Laboratorio externo responde la solicitud con una respuesta factible.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-05",
+      descripcion:
+        "Técnico DNM recibe la respuesta de la cotización del laboratorio externo.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-05",
+      descripcion:
+        "Técnico DNM envía las muestras necesarias al laboratorio externo.",
+    },
+  
+    {
+      responsable: "Laboratorio externo",
+      fecha: "2022-10-05",
+      descripcion:
+        "Laboratorio externo recibe las muestras para su debido análisis.",
+    },
+  
+    //analisis en proceso
+  
+    /* {
+      mensaje: "Análisis en proceso.",
+    }, */
+  
+    {
+      responsable: "Laboratorio externo",
+      fecha: "2022-10-31",
+      descripcion:
+        "Laboratorio externo finaliza el análisis y envía los resultados al Técnico DNM",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-31",
+      descripcion:
+        "Técnico DNM recibe mediante una solicitud los resultados del análisis de laboratorio.",
+    },
+  
+    {
+      responsable: "Técnico DNM",
+      fecha: "2022-10-31",
+      descripcion:
+        "Técnico DNM envía los resultados a la ventanilla electrónica DNM.",
+    },
+  
+    /* {
+      mensaje2: "Proceso finalizado.",
+    }, */
+  
+    //proceso finalizado
+  ];
+
+movimientoCard.classList.add('row', 'row-1');
+
+let template = ``;
+
+movimientos.forEach((movimiento) => {
+    template = `
+    <section class="card">
+        <i class="icon fas fa-home"></i>
+        <div class="details">
+            <span class="title">${ movimiento.responsable }</span>
+            <span>${ movimiento.fecha }</span>
+        </div>
+        <p>${ movimiento.descripcion }</p>
+    </section>
+    `;
+
+    movimientoCard.innerHTML += template;
+})
+
+container.append(movimientoCard);
 
 let filter = "DESC";
 
 btn.addEventListener("click", cambiarOrden);
-
-const movimientos = [
-  {
-    responsable: "Ventanilla Electrónica",
-    fecha: "2022-10-03",
-    descripcion:
-      "Ventanilla Electrónica envió una solicitud al sistema de pruebas con laboratorios externos.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-03",
-    descripcion:
-      "Técnico DNM recibió una solicitud desde Ventanilla Electrónica.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-03",
-    descripcion: "Técnico DNM acepta la solicitud.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-03",
-    descripcion:
-      "Técnico DNM elige laboratorio para realizar la prueba de análisis.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-03",
-    descripcion:
-      "Técnico DNM envía solicitud de cotización a laboratorio externo.",
-  },
-
-  {
-    responsable: "Laboratorio externo",
-    fecha: "2022-10-03",
-    descripcion: "Laboratorio externo recibe solicitud de cotización.",
-  },
-
-  {
-    responsable: "Laboratorio externo",
-    fecha: "2022-10-03",
-    descripcion:
-      "Laboratorio externo verifica la factibilidad de la prueba a realizar análisis de laboratorio.",
-  },
-
-  {
-    responsable: "Laboratorio externo",
-    fecha: "2022-10-05",
-    descripcion:
-      "Laboratorio externo responde la solicitud con una respuesta factible.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-05",
-    descripcion:
-      "Técnico DNM recibe la respuesta de la cotización del laboratorio externo.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-05",
-    descripcion:
-      "Técnico DNM envía las muestras necesarias al laboratorio externo.",
-  },
-
-  {
-    responsable: "Laboratorio externo",
-    fecha: "2022-10-05",
-    descripcion:
-      "Laboratorio externo recibe las muestras para su debido análisis.",
-  },
-
-  //analisis en proceso
-
-  /* {
-    mensaje: "Análisis en proceso.",
-  }, */
-
-  {
-    responsable: "Laboratorio externo",
-    fecha: "2022-10-31",
-    descripcion:
-      "Laboratorio externo finaliza el análisis y envía los resultados al Técnico DNM",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-31",
-    descripcion:
-      "Técnico DNM recibe mediante una solicitud los resultados del análisis de laboratorio.",
-  },
-
-  {
-    responsable: "Técnico DNM",
-    fecha: "2022-10-31",
-    descripcion:
-      "Técnico DNM envía los resultados a la ventanilla electrónica DNM.",
-  },
-
-  /* {
-    mensaje2: "Proceso finalizado.",
-  }, */
-
-  //proceso finalizado
-];
 
 function cambiarOrden() {
   let movNew;
